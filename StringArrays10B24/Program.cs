f8b6ec60c2d918bbb2926cee229f33339b5eaf00
jbuck <jbuck@S029A-SD-01>
2024-10-23 11:52:50 +0100

    Add project files.

---
+++ StringArrays10B24/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StringArrays10B24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Write a subroutine called Prefix from your pseudo-code 
+             * The subroutine takes two, character arrays called word and pre as parameters and 
+             * The subroutine determines whether the start of the character array word is the 
+             * same as all of the character array pre.*/
+
+            /* Your subroutine should also:
+	•           work for character arrays of all lengths greater than 0
+	•           not assume that the length of pre is less than the length of word.*/
+
+            /* use fixed (hard coded) string arrays to test your code. 
+             Call your subroutine using your test arrays from Main() routine
+             and return the result to Main() */
+            
+            /* Starter code for your subroutine has been provided*/
+
+        }
+        static bool Prefix(string[] wor, string[] pre) 
+        {
+
+            return;
+        }
+    }
+}
